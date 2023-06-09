eae30a8ba99bd798cdee22a85d82ea457166c848
Harlon Garcia <harlondesigner@gmail.com>
2023-06-08 21:34:16 -0300

    feat: add page routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import AdminPanel from "./pages/AdminPanel";
 
 function App() {
   return (
-    <>
-    </>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<AdminPanel />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
